e5e8347825353e0849fa9e079609868ceacf7b15
joao <joaovitorgiroldi@gmail.com>
2022-01-19 18:32:49 -0300

    meu primeiro arquivo

---
+++ Projetos/Projeto 1/Projeto01.js
@@ -0,0 +1,46 @@
+const entrada = require('prompt-sync')();
+console.clear
+console.log(`História`)
+
+let Q1 = entrada(`Pergunta 1 `)
+let Q2 = entrada(`Pergunta 2 `)
+let Q3 = entrada(`Pergunta 3 `)
+let Q4 = entrada(`Pergunta 4 `)
+let Q5 = entrada(`Pergunta 5 `)
+
+var contador = 0
+
+if ( Q1.toUpperCase() == 'SIM'){
+    contador++
+}
+if ( Q2.toUpperCase() == 'SIM'){
+    contador++
+}
+if ( Q3.toUpperCase() == 'SIM'){
+    contador++
+}
+if ( Q4.toUpperCase() == 'SIM'){
+    contador++
+}
+if ( Q5.toUpperCase() == 'SIM'){
+    contador++
+}
+
+if (contador == 0 ){
+
+}
+if (contador == 1 ){
+
+}
+if (contador == 2 ){
+
+}
+if (contador == 3 ){
+
+}
+if (contador == 4 ){
+
+}
+if (contador == 5 ){
+
+}
